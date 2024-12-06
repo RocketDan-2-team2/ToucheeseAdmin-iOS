a864dd4ad195e20e55e5404f4448b839a49a5a37
JaeWoongJeong <jjw8959@naver.com>
2024-12-06 14:30:23 +0900

    feat: 프로젝트 추가

---
+++ ToucheeseAdmin/ToucheeseAdmin/ToucheeseAdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToucheeseAdminApp.swift
+//  ToucheeseAdmin
+//
+//  Created by woong on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ToucheeseAdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
