56fc2fd9774af233b8109a49f5a6b3bbd92eb397
JaeWoongJeong <jjw8959@naver.com>
2024-12-23 13:50:39 +0900

    feat: 에러타입 생성

---
+++ ToucheeseAdmin/ToucheeseAdmin/Network/ErrorTypes.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkError.swift
+//  ToucheeseAdmin
+//
+//  Created by woong on 12/23/24.
+//
+
+import Foundation
+
+enum ErrorTypes: Error {
+    case invalidURL
+    case noData
+    case decodingError
+    case serverError(statusCode: Int)
+}
